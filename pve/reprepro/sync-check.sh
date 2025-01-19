78dd87ecb72f202c6d6563f8349223ddb8fad47e
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-01-19 14:54:25 -0800

    update

--- pve/reprepro/sync-check.sh
+++ pve/reprepro/sync-check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ################################################################################
 #
-# sync_check
+# sync-check
 # ----------------
-# Check if Gridcoin wallet is in sync
+# Check if deb files are in sync
 #
 # @author Nicholas Wilde, 0x08b7d7a3
-# @date 27 Jul 2024
+# @date 19 Jan 2025
 # @version 0.1.0
 #
 ################################################################################
@@ -15,49 +15,78 @@
 # set -o pipefail
 
 # Set the URL for the GitHub releases API
-API_URL="https://api.github.com/repos/go-task/task/releases/latest"
 archs=(amd64 arm64)
-codenames=(bullseye bookworm)
+debian_codenames=(bullseye bookworm)
+ubuntu_codenames=(noble oracular)
+usernames=(getsops go-task)
+apps=(sops task)
+
+function get_params(){
+  username="${usernames[i]}"
+  export username
+}
+
+function get_api_url(){
+  api_url="https://api.github.com/repos/${username}/${app}/releases/latest"
+  export api_url
+}
 
 # Get the latest release version from the API
 function get_latest_version(){
-  LATEST_VERSION=$(curl -s "$API_URL" | grep '"tag_name":' | cut -d '"' -f 4)
+  latest_version=$(curl -s "$api_url" | grep '"tag_name":' | cut -d '"' -f 4)
   # Remove the "v" prefix from the version string
-  LATEST_VERSION=${LATEST_VERSION#v}
-  export LATEST_VERSION
+  latest_version2=${latest_version#v}
+  export latest_version
+  export latest_version2
 }
 
 function get_current_version(){
   # Get the current installed version (if any)
-  CURRENT_VERSION=$(task --version 2>/dev/null | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-  export CURRENT_VERSION
+  current_version=$("${app}" --version 2>/dev/null | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+  export current_version
+}
+
+function get_filename(){
+  export filename
 }
 
 function check_version(){
   # Compare versions
-  if [[ "$LATEST_VERSION" != "$CURRENT_VERSION" ]]; then
-    echo "New version available: $LATEST_VERSION"
-
-    # Download the .deb package
-    PACKAGE_URL="https://github.com/go-task/task/releases/download/${LATEST_VERSION}/task_linux_arm64.deb"
-    wget "${PACKAGE_URL}"
-
-    message="Downloaded task_linux_arm64.deb"
-
+  if [[ "${latest_version2}" != "${current_version}" ]]; then
+    printf 'New version available: %s\n' "${latest_version2}"
+    for arch in "${archs[@]}"; do
+      export arch
+      printf '%s\n' "${arch}"
+      # Download the .deb package
+      # package_url="https://github.com/go-task/task/releases/download/${latest_version}/task_linux_arm64.deb"
+      # wget "${package_url}"
+# 
+      # message="Downloaded task_linux_arm64.deb"
+    done
     # (Optional) Install the package
     # sudo dpkg -i task_linux_arm64.deb
 
   else
-    message="Already up-to-date: ${CURRENT_VERSION}"
+    message="Already up-to-date: ${current_version}"
   fi
   echo "${message}"
   logger -t "sync_check" "${message}"
 }
 
 function main(){
-  get_latest_version
-  get_current_version
-  check_version
+  i=0
+  for app in "${apps[@]}"; do
+    export app
+    export i
+    get_params
+    get_api_url
+    get_latest_version
+    get_current_version
+    check_version
+    printf '%s\n' "${current_version}"
+    # printf '%s\n' "${latest_version2}"
+    ((i+=1))
+  done
 }
 
 main "$@"
