ecc864cda5c3aa57b353fd54b22931ad37d2947d
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-02-13 13:00:12 -0800

    update
    Signed-off-by: nιcнolaѕ wιlde <ncwilde43@gmail.com>

--- pve/reprepro/sync-check.sh
+++ pve/reprepro/sync-check.sh
@@ -51,26 +51,8 @@ function is_set(){
   [ -n "${1}" ]
 }
 
-# Check if directory exists
-function dir_exists(){
-  [ -d "${1}" ]
-}
-
 function command_exists() {
   command -v "$1" >/dev/null 2>&1
-}
-
-function set_vars(){
-  SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-  [ -f "${SCRIPT_DIR}/.env" ] && source "${SCRIPT_DIR}/.env"
-  API_URL="https://api.github.com/repos/${2}/${1}/releases/latest"
-  export API_URL
-  # curl -s https://api.github.com/repos/go-task/task/releases/latest | jq -r '.assets[] | select(.name | contains("amd64.deb")) | .browser_download_url'
-  PACKAGE_URL="https://github.com/${2}/${1}/releases/download/${LATEST_VERSION}/task_linux_arm64.deb"
-  print_text "${API_URL}"
-  export PACKAGE_URL
-  # ! is_set "${VENTOY_INTALL_DIR}" && VENTOY_INSTALL_DIR="${DEFAULT_VENTOY_INSTALL_DIR}"
-  # ! is_set "${FILENAME}" && FILENAME="${DEFAULT_FILENAME}"
 }
 
 function check_reprepro(){
@@ -78,6 +60,45 @@ function check_reprepro(){
     raise_error "reprepro is not installed"
   fi
 }
+
+function check_jq(){
+  if ! command_exists "jq"; then
+    raise_error "jq is not installed"
+  fi
+}
+
+function check_root(){
+  if [ "$UID" -ne 0 ]; then
+    raise_error "Please run as root or with sudo."
+  fi
+}
+
+function make_temp_dir(){
+  TEMP_PATH=$(mktemp -d)
+  [ -d "${TEMP_PATH}" ] || raise_error "Could not create temp dir"
+  export TEMP_PATH
+  print_text "Temp path: ${TEMP_PATH}"
+}
+
+function set_vars(){
+  SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+  [ -f "${SCRIPT_DIR}/.env" ] && source "${SCRIPT_DIR}/.env"
+  API_URL="https://api.github.com/repos/${2}/${1}/releases/latest"
+  PACKAGE_URL_AMD64=$(curl -s "${API_URL}" | jq -r '.assets[] | select(.name | contains("amd64.deb")) | .browser_download_url')
+  PACKAGE_URL_ARM64=$(curl -s "${API_URL}" | jq -r '.assets[] | select(.name | contains("arm64.deb")) | .browser_download_url')
+  BASENAME_AMD64=$(basename "${PACKAGE_URL_AMD64}")
+  BASENAME_ARM64=$(basename "${PACKAGE_URL_ARM64}")
+  FILEPATH_AMD64="${TEMP_PATH}/${BASENAME_AMD64}"
+  FILEPATH_ARM64="${TEMP_PATH}/${BASENAME_ARM64}"
+  export API_URL
+  export PACKAGE_URL_AMD64
+  export PACKAGE_URL_ARM64 
+  export BASENAME_ARM64
+  export BASENAME_AMD64
+  export FILEPATH_AMD64
+  export FILEPATH_ARM64
+}
+
 
 # Get the latest release version from the API
 function get_latest_version(){
@@ -90,26 +111,34 @@ function get_latest_version(){
 }
 
 function get_current_version(){
-  print_text "Get current version"
   APP_NAME="${1}"
   CURRENT_VERSION=$(reprepro --confdir /srv/reprepro/ubuntu/conf/ list oracular "${APP_NAME}" | grep 'amd64'| awk '{print $NF}')
   export CURRENT_VERSION
   print_text "Current version: ${CURRENT_VERSION}"
 }
 
+function add_package(){
+  PACKAGE_URL="${1}"
+  FILEPATH="${2}"
+  wget "${PACKAGE_URL}" -O "${FILEPATH}"
+  reprepro --confdir /srv/reprepro/ubuntu/conf/ includedeb oracular "${FILEPATH}"
+  reprepro --confdir /srv/reprepro/ubuntu/conf/ includedeb noble "${FILEPATH}"
+  reprepro --confdir /srv/reprepro/debian/conf/ includedeb bookworm "${FILEPATH}"
+  reprepro --confdir /srv/reprepro/debian/conf/ includedeb bullseye "${FILEPATH}"
+}
+
 function check_version(){
   # Compare versions
   if [[ "${LATEST_VERSION2}" != "${CURRENT_VERSION}" ]]; then
-    printf 'New version available: %s\n' "${LATEST_VERSION2}"
-      # package_url="https://github.com/go-task/task/releases/download/${LATEST_VERSION}/task_linux_arm64.deb"
-      # wget "${package_url}"
- 
-      # message="Downloaded task_linux_arm64.deb"
+    print_text "New version available: ${LATEST_VERSION2}"
+    add_package "${PACKAGE_URL_AMD64}" "${FILEPATH_AMD64}"
+    add_package "${PACKAGE_URL_ARM64}" "${FILEPATH_ARM64}" 
+    MESSAGE="Downloaded ${APP_NAME} deb files"
   else
-    message="Already up-to-date: ${current_version}"
+    MESSAGE="is slready up-to-date: ${current_version}"
   fi
-  echo "${message}"
-  logger -t "sync_check" "${message}"
+  print_text "${MESSAGE}"
+  logger -t "sync-check" "${MESSAGE}"
 }
 
 function update_app(){
@@ -118,10 +147,14 @@ function update_app(){
   get_current_version "${APP_NAME}"
   set_vars "${APP_NAME}" "${USERNAME}"
   get_latest_version "${APP_NAME}"
+  check_version
 }
 
 function main(){
+  check_root
   check_reprepro
+  check_jq
+  make_temp_dir
   update_app "task" "go-task"
   # update_app "sops" "go-task"
 }
