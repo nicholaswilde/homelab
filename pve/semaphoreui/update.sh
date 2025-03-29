bc55f2f8c08954a7dde5f7d4f29810e8fc975690
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-03-29 12:27:12 -0700

    Update update.sh

--- pve/semaphoreui/update.sh
+++ pve/semaphoreui/update.sh
@@ -1,33 +1,71 @@
+#!/usr/bin/env bash
 
+################################################################################
+#
+# update
+# ----------------
+# Update Semaphore
+#
+# @author Nicholas Wilde, 0x08b7d7a3
+# @date 29 Mar 2025
+# @version 0.1.0
+#
+################################################################################
+
+# set -e
+# set -o pipefail
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+red=$(tput setaf 1)
+blue=$(tput setaf 4)
+default=$(tput setaf 9)
+white=$(tput setaf 7)
+
+readonly bold
+readonly normal
+readonly red
+readonly blue
+readonly default
+readonly white
+
+function print_text(){
+  echo "${blue}==> ${white}${bold}${1}${normal}"
+}
+
+function raise_error(){
+  printf "${red}%s\n" "${1}"
+  exit 1
+}
 
 function update_script() {
+  APP="Semaphore"
   if [[ ! -f /etc/systemd/system/semaphore.service ]]; then
-    msg_error "No ${APP} Installation Found!"
-    exit
+    raise_error "No ${APP} Installation Found!"
   fi
   RELEASE=$(curl -s https://api.github.com/repos/semaphoreui/semaphore/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
   if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
-    msg_info "Stopping Service"
+    print_text "Stopping Service"
     systemctl stop semaphore
-    msg_ok "Stopped Service"
+    print_text "Stopped Service"
 
-    msg_info "Updating ${APP} to v${RELEASE}"
+    print_text "Updating ${APP} to v${RELEASE}"
     cd /opt
-    wget -q https://github.com/semaphoreui/semaphore/releases/download/v${RELEASE}/semaphore_${RELEASE}_linux_amd64.deb
-    $STD dpkg -i semaphore_${RELEASE}_linux_amd64.deb
+    wget -q https://github.com/semaphoreui/semaphore/releases/download/v${RELEASE}/semaphore_${RELEASE}_linux_arm64.deb
+    $STD dpkg -i semaphore_${RELEASE}_linux_arm64.deb
     echo "${RELEASE}" >"/opt/${APP}_version.txt"
-    msg_ok "Updated ${APP} to v${RELEASE}"
+    print_text "Updated ${APP} to v${RELEASE}"
 
-    msg_info "Starting Service"
+    print_text "Starting Service"
     systemctl start semaphore
-    msg_ok "Started Service"
+    print_text "Started Service"
 
-    msg_info "Cleaning up"
-    rm -rf /opt/semaphore_${RELEASE}_linux_amd64.deb
-    msg_ok "Cleaned"
-    msg_ok "Updated Successfully"
+    print_text "Cleaning up"
+    rm -rf /opt/semaphore_${RELEASE}_linux_arm64.deb
+    print_text "Cleaned"
+    print_text "Updated Successfully"
   else
-    msg_ok "No update required. ${APP} is already at v${RELEASE}."
+    print_text "No update required. ${APP} is already at v${RELEASE}."
   fi
 }
 
