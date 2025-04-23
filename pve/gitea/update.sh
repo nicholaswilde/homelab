b6ec0b2d344c610ebd720ea49bdf95ba62a8cba8
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-04-23 09:33:31 -0700

    Update update.sh

--- pve/gitea/update.sh
+++ pve/gitea/update.sh
@@ -60,12 +60,12 @@ function update_script() {
    RELEASE=$(curl -fsSL https://github.com/go-gitea/gitea/releases/latest | grep "title>Release" | cut -d " " -f 4 | sed 's/^v//')
    msg_info "Updating $APP to ${RELEASE}"
    curl -fsSL "https://github.com/go-gitea/gitea/releases/download/v$RELEASE/gitea-$RELEASE-linux-amd64" -o $(basename "https://github.com/go-gitea/gitea/releases/download/v$RELEASE/gitea-$RELEASE-linux-amd64")
-   systemctl stop gitea
-   rm -rf /usr/local/bin/gitea
-   mv gitea* /usr/local/bin/gitea
-   chmod +x /usr/local/bin/gitea
-   systemctl start gitea
-   msg_ok "Updated $APP Successfully"
+   sudo systemctl stop gitea
+   sudo rm -rf /usr/local/bin/gitea
+   sudo mv gitea* /usr/local/bin/gitea
+   sudo chmod +x /usr/local/bin/gitea
+   sudo systemctl start gitea
+   print_text "Updated gitea Successfully"
    exit 0
 }
 
