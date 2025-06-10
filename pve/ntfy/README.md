4c5ccf8ebf3ae5d50e9e79fbeea5ed427acaf54e
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-06-10 02:48:56 +0000

    feat: add ntfy
    Signed-off-by: nιcнolaѕ wιlde <ncwilde43@gmail.com>

---
+++ pve/ntfy/README.md
@@ -0,0 +1,5 @@
+# ntfy
+
+See documentation located [here][1].
+
+[1]: <https://nicholaswilde.io/homelab/apps/ntfy/>
---
+++ pve/ntfy/update.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+################################################################################
+#
+# ntfy
+# ----------------
+# Update ntfy
+#
+# @author Nicholas Wilde, 0xb299a622
+# @date 09 Jun 2025
+# @version 0.1.0
+#
+################################################################################
+
+# set -e
+# set -o pipefail
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+red=$(tput setaf 1)
+blue=$(tput setaf 4)
+default=$(tput setaf 9)
+white=$(tput setaf 7)
+yellow=$(tput setaf 3)
+
+readonly bold
+readonly normal
+readonly red
+readonly blue
+readonly default
+readonly white
+readonly yellow
+readonly APP
+
+function print_text(){
+  echo "${blue}==> ${white}${bold}${1}${normal}"
+}
+
+function show_warning(){
+  printf "${yellow}%s\n" "${1}${normal}"
+}
+
+function raise_error(){
+  printf "${red}%s\n" "${1}${normal}"
+  exit 1
+}
+
+# Check if variable is set
+# Returns false if empty
+function is_set(){
+  [ -n "${1}" ]
+}
+
+function command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+function check_url(){
+  local url="${1}"
+  local status=$(curl -sSL -o /dev/null -w "${http_code}" "${url}")
+  if [[ "${status}" -ge 200 && "${status}" -lt 400 ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function check_curl(){
+  if ! command_exists curl; then
+    raise_error "curl is not installed"
+  fi
+}
+
+function update_script() {
+  APP=ntfy
+  if ! command_exists ntfy; then
+    raise_error "No ntfy Installation Found!"
+  fi
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(armhf\)\(64\)\?.*/\1\2hf/' -e 's/aarch64$/arm64/')"
+  # RELEASE=$(curl -fsSL https://github.com/go-gitea/ntfy/releases/latest | grep "title>Release" | cut -d " " -f 4 | sed 's/^v//')
+  RELEASE=$(curl -fsSL https://api.github.com/repos/go-gitea/ntfy/releases/latest | grep -o '"tag_name": *"[^"]*"' | cut -d '"' -f 4 | sed 's/^v//')
+  print_text "Updating ${APP} to ${RELEASE}"
+  curl -fsSL "https://github.com/go-gitea/ntfy/releases/download/v$RELEASE/gitea-$RELEASE-linux-${ARCH}" -o $(basename "https://github.com/go-gitea/ntfy/releases/download/v$RELEASE/gitea-$RELEASE-linux-${ARCH}")
+  sudo systemctl stop ntfy
+  sudo rm -rf /usr/local/bin/ntfy
+  sudo mv gitea* /usr/local/bin/ntfy
+  sudo chmod +x /usr/local/bin/ntfy
+  sudo systemctl start ntfy
+  print_text "Updated ntfy Successfully"
+}
+
+function main(){
+  check_curl
+  update_script
+}
+
+main "@"
