106a088a9fc3fdaea275d75f5cc9f90cf353f780
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-10-12 07:29:47 +0000

    update
    Signed-off-by: nιcнolaѕ wιlde <ncwilde43@gmail.com>

--- pve/reprepro/package-apps.sh
+++ pve/reprepro/package-apps.sh
@@ -20,6 +20,7 @@
 readonly BLUE=$(tput setaf 4)
 readonly RED=$(tput setaf 1)
 readonly YELLOW=$(tput setaf 3)
+readonly PURPLE=$(tput setaf 5)
 readonly RESET=$(tput sgr0)
 readonly SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 readonly DEBIAN_CODENAMES=($(grep -oP '(?<=Codename: ).*' "${SCRIPT_DIR}/debian/conf/distributions"))
@@ -54,6 +55,8 @@ function log() {
       color="$YELLOW";;
     ERRO)
       color="$RED";;
+    DEBU)
+      color="$PURPLE";;
   esac
 
   echo -e "${color}${type}${RESET}[$(date +'%Y-%m-%d %H:%M:%S')] ${message}"
@@ -149,11 +152,16 @@ function remove_package() {
 function package_and_add() {
   local arch_github=$1
   local arch_debian=$2
-
+  local tarball_name=$3
+  
   log "INFO" "Processing architecture: ${arch_github}"
 
-  local download_url="https://github.com/${GITHUB_REPO}/releases/download/${TAG_NAME}/${APP_NAME}-${LATEST_VERSION}-linux_${arch_github}.tar.gz"
-  local tarball_name="${APP_NAME}-${LATEST_VERSION}-linux_${arch_github}.tar.gz"
+  local download_url=$(echo "${json_response}" | jq -r --arg pkg_name "$tarball_name" '.assets[] | select(.name==$pkg_name) | .browser_download_url')
+  if [ -z "${download_url}" ]; then
+    log "ERRO" "Failed to get download url for ${package_name}"
+    return 1
+  fi
+    
   local tarball_path="${TEMP_PATH}/${tarball_name}"
 
   log "INFO" "Downloading ${tarball_name}..."
@@ -183,12 +191,6 @@ Maintainer: Nicholas Wilde <noreply@email.com>
 Description: ${DESCRIPTION}
 EOF
 
-  # Define the output target based on the DEBUG variable
-  if [ "${DEBUG}" = "true" ]; then
-    OUTPUT_TARGET="/dev/stdout" # Send output to the screen
-  else
-    OUTPUT_TARGET="/dev/null"   # Send output to the void
-  fi
   log "INFO" "Building .deb package..."
   local deb_file="${APP_NAME}_${LATEST_VERSION}_${arch_debian}.deb"
   if ! dpkg-deb --build "${package_dir}" "${TEMP_PATH}/${deb_file}" &> "${OUTPUT_TARGET}"; then
@@ -228,15 +230,17 @@ function update_app() {
 
   log "INFO" "New version available: ${LATEST_VERSION}"
 
-  remove_package "${APP_NAME}"
+  # remove_package "${APP_NAME}"
+
+  export DESCRIPTION=$(curl -s "https://api.github.com/repos/${GITHUB_REPO}" | jq -r '.description' | sed -e 's/:\w\+://g' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
   local linux_tarballs
-  linux_tarballs=$(echo "${json_response}" | jq -r '.assets[] | select(.name | test("linux.*.tar.gz$")) | .name')
+  linux_tarballs=$(echo "${json_response}" | jq -r '.assets[] | select(.name | endswith(".tar.gz") and (contains("musl") | not) and (contains("openbsd") | not) and (contains("darwin") | not) and (contains("freebsd")| not) and (contains("android") | not)) | .name')
 
   for tarball in ${linux_tarballs}; do
     local github_arch
-    github_arch=$(echo "${tarball}" | sed -n "s/${APP_NAME}-${LATEST_VERSION}-linux_\(.*\)\.tar\.gz/\1/p")
-
+    github_arch=$(echo "${tarball}" | grep -oP '(?<=_)[^_]+(?=\.tar\.gz)')
+    
     local debian_arch=""
     case "${github_arch}" in
       "amd64")
@@ -247,12 +251,16 @@ function update_app() {
         debian_arch="armhf";;
       "386")
         debian_arch="i386";;
+      "armhf")
+        debian_arch="armhf";;
+      "arm")
+        debian_arch="arm";;
       *)
         log "WARN" "Unsupported architecture found: ${github_arch}. Skipping."
         continue;;
     esac
 
-    if ! package_and_add "${github_arch}" "${debian_arch}"; then
+    if ! package_and_add "${github_arch}" "${debian_arch}" "${tarball}"; then
       log "ERRO" "Skipping ${APP_NAME} ${github_arch} due to packaging error."
       continue
     fi
