a0fcaafe75d1087d6a4e1c155e7ae9d978a96eb1
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2025-11-10 08:29:21 -0800

    Update backup-adguardhome.sh

--- pve/adguardhome/backup-adguardhome.sh
+++ pve/adguardhome/backup-adguardhome.sh
@@ -96,8 +96,7 @@ function send_notification(){
   if [ ${#UPDATE_MESSAGES[@]} -gt 0 ]; then
     EMAIL_BODY+=$'\n\nUpdate details:\n'
     for msg in "${UPDATE_MESSAGES[@]}"; do
-      EMAIL_BODY+="- ${msg}"$'
-'
+      EMAIL_BODY+="- ${msg}"
     done
   fi
 
